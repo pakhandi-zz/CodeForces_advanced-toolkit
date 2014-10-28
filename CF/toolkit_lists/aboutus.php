b243179cab6652680a9f342f5f75413d4c48a74d
pakhandi <asimkprasad@gmail.com>
2014-10-29 01:15:14 +0530

    AboutUs section

---
+++ CF/toolkit_lists/aboutus.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>CF-Tools</title>
+<style type="text/css">
+
+</style>
+<link rel='shortcut icon' type='image/x-icon' href='../../images/favicon.ico' />
+<link href="../../css/profilelist.css" rel="stylesheet" type="text/css" media="screen"/>
+<link href="mast.css" rel="stylesheet" type="text/css" media="screen"/>
+<style>
+
+#trans
+{
+  padding-left: 15px;
+  padding-top: 50px;
+  padding-right: 15px;
+  border: 1px solid rgba(255, 255, 255, 0.4);
+  box-shadow: 0px 0px 30px 0px rgba(50, 50, 50, 0.32);
+  background-color: rgba(255, 255, 255, 0.2);
+  background: rgba(231, 238, 231, 0.2);
+  opacity: 1 !important;
+  font-size:  : 20px !important;
+}
+
+#back_body{
+  height: 150%;
+}
+#upper_body{
+  height: 7%;
+  position: fixed;
+  z-index: 10;
+}
+#lower{
+  position: fixed;
+}
+body
+{
+  position: fixed;
+}
+
+#name1
+{
+  position: absolute;
+  top: 340px;
+  left: 130px;
+  font-size: 18px !important;
+  width: 200px;
+
+}
+#name2
+{
+  position: absolute;
+  top: 340px;
+  left: 1070px;
+  font-size: 18px !important;
+  width: 200px;
+}
+
+</style>
+</head>
+
+<body>
+<div class="back_body" id="back_body">
+  <div class="upper_body" id="upper_body">
+    <div class="logo" id="logo">
+        <a href="index.php">
+          <font id="logotext" class="logotext" style="font-family:sans-serif;color:red;">
+              CF-List
+          </font>
+        </a>
+    </div>
+
+  </div>
+  <div class="pic1" id="pic1">
+      <a href="https://www.facebook.com/AsimKPrasad" target="_blank"><img src="http://codeforces.com/userphoto/title/pakhandi/photo.jpg" height="190px" width="190px"></a>
+  </div>
+  
+  <div class="pic6" id="pic6">
+      <a href="https://www.facebook.com/mpsmks" target="_blank"><img src="http://codeforces.com/userphoto/title/mmaks/photo.jpg" height="190px" width="190px"></a>
+  </div>
+
+  <div id="name1">
+    <a href="https://www.facebook.com/AsimKPrasad" target="_blank">Asim Krishna Prasad</a>
+    </div>
+  <div id="name2">
+    <a href="https://www.facebook.com/mpsmks" target="_blank">Manish Kumar Sinha</a>
+  </div>
+  <div class="trans" id="trans">
+    <center style="font-size: 20px;">
+    We are a team of passionate, crazy & lazy night-crawlers <b>:P</b> <br>
+    2nd Year UnderGrads.. Trying to test our hands on some basic web-development..<br>
+    Hope our tiny effort proves helpful for you..<br>
+    We would like to thank CodeForces & Bootstrap for such great tools and platforms and then ofcourse our seniors <b>:)</b>.. <br>
+    For hugs || Bugs drop a mail at : <br> <u>asimkprasad@gmail.com</u> || <u>mksinha1994@gmail.com</u>.. <b>:)</b> <br>
+    Gl & Hf.. <b>;)</b>
+  </center>
+  </div>
+  <div class="mast" id="mast">
+    
+      
+    
+  </div>
+  
+  
+  <?php include "../../include/lowerbodymaindir.php"; ?>
+
+  
+</div>
+</body>
+</html>
+		
