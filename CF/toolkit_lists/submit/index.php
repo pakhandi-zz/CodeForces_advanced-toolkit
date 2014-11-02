7b2957874afd0b1f1af1485580db27c26cbf0723
pakhandi <asimkprasad@gmail.com>
2014-11-03 00:44:08 +0530

    Registration

---
+++ CF/toolkit_lists/submit/index.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+
+  This page has the registration form with captcha
+  and the facility to check the availability of the username..
+
+
+*/
+
+?>
---
+++ CF/toolkit_lists/submit/submitprofile.php
@@ -0,0 +1,13 @@
+<?php
+
+/*
+
+  This page contains the code to register a new user
+  and make the entry in the data base..
+
+  This page actually sends the mail to the registered
+  mail-id..
+
+*/
+
+?>
