5bd1da01af85689f82bbecca2e5a7b35c2f13b92
pakhandi <asimkprasad@gmail.com>
2014-11-20 06:46:47 +0530

    Added Tooltip
    Basic Tooltip using CSS

--- include/upper_body_buttons.php
+++ include/upper_body_buttons.php
@@ -1,6 +1,40 @@
 	<div id="new_btns">
-        <a href="http://tools.bugecode.com/CF/toolkit_lists/inex.php" target="_blank"><button id="b1" class="b_new_btns btn btn-lg btn-success">Compete</button></a>
-        <a href="http://tools.bugecode.com/CF/toolkit_lists/allprobs.php" target="_blank"><button id="b2" class="b_new_btns btn btn-lg btn-success">All Submissions</button></a>
-        <a href="http://tools.bugecode.com/CF/toolkit_lists/submissions.php" target="_blank"><button id="b3" class="b_new_btns btn btn-lg btn-success">Last Submissions</button></a>
-        <a href="http://tools.bugecode.com/CF/toolkit_lists/cprobs.php" target="_blank"><button id="b4" class="b_new_btns btn btn-lg btn-success">Compare</button></a>
+        <a href="http://tools.bugecode.com/CF/toolkit_lists/inex.php">
+        	<button id="b1" class="b_new_btns btn btn-lg btn-success">
+        		Compete
+        	</button>
+        	<div class="show_help" id="b1_help">
+        		<b>Here you can compare the statistics of two users..</b>
+        	</div>
+        </a>
+
+        <a href="http://tools.bugecode.com/CF/toolkit_lists/allprobs.php">
+        	<button id="b2" class="b_new_btns btn btn-lg btn-success">
+        		All Submissions
+        	</button>
+        	<div class="show_help" id="b2_help">
+        		<b>You can see all the AC questions/solutions for any user..</b>
+        	</div>
+        </a>
+        <a href="http://tools.bugecode.com/CF/toolkit_lists/submissions.php">
+        	<button id="b3" class="b_new_btns btn btn-lg btn-success">
+        		Last Submissions
+        	</button>
+        	<div class="show_help" id="b3_help">
+        		<b>Here you can see the last 15 submissions by any user..</b>
+        	</div>
+        </a>
+        <a href="http://tools.bugecode.com/CF/toolkit_lists/cprobs.php">
+        	<button id="b4" class="b_new_btns btn btn-lg btn-success">
+        		Compare
+        	</button>
+        	<div class="show_help" id="b4_help">
+        		<b>This is the most fun part.. Here you can actually compare the AC solutions by two users for common AC-problems..</b>
+        	</div>
+        </a>
+        <a href="http://www.bugecode.com">
+        	<button id="b5" class="b_new_btns b_new_btns_danger btn btn-lg btn-danger">
+        		Bug-e-Code
+        	</button>
+        </a>
     </div>
